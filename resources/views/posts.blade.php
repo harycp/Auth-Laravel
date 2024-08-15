7b4dc68ce0fd891c88d9cbad870adbbf85c9a157
Hary Capri <76942747+harycp@users.noreply.github.com>
2024-08-15 20:38:34 +0700

    add blog page

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,16 @@
+<x-layout>
+    <x-slot:title> {{ $title }}</x-slot>
+
+    @foreach ($posts as $post)
+        <article class="py-6 max-w-screen-md border-b-2 border-gray-400">
+            <h2 class="mb-1 text-2xl font-bold tracking-tight text-gray-900">{{ $post['title'] }}</h2>
+            <div class="text-gray-500 text-base mb-4">
+                <a href="#">{{ $post['author'] }}</a> | <time>May 22, 2021</time>
+            </div>
+            <p class="mb-4 font-light">{{ Str::limit($post['content'], 70) }}</p>
+            <a href="#" class="font-medium text-purple-900 hover:underline">Read more &raquo;</a>
+        </article>
+    @endforeach
+
+
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,27 @@ Route::get('/about', function (){
     return view('about', ['title' => 'About']);
 });
 
-Route::get('/blog', function (){
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function (){
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'title' => 'Functionality of VueJS',
+            'author' => 'Hary Capri',
+            'content' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero iure recusandae
+            exercitationem
+            voluptatibus
+            quia veritatis quod reiciendis facilis eaque mollitia. Facere ea nobis earum. Officiis, asperiores.
+            Distinctio placeat quam natus?Corrupti amet deleniti iste? Veniam dolores, distinctio consectetur impedit
+            officiis sint recusandae. Magni distinctio, repellat nihil, dolor reiciendis maxime explicabo inventore
+            ipsam libero illo molestiae optio deserunt aperiam tenetur. Magnam?'
+        ],
+        [
+            'title' => 'What is React JS ?',
+            'author' => 'Hary Capri',
+            'content' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam est repudiandae
+            praesentium similique et quia dolores voluptas blanditiis nihil eligendi modi corrupti aperiam voluptatibus
+            consequatur, explicabo assumenda magnam ex neque!'
+        ]
+    ]]);
 });
 
 Route::get('/contact', function (){
