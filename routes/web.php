a0244e71a61f36bf175d05905b51c96ca41c784c
Hary Capri <76942747+harycp@users.noreply.github.com>
2024-08-15 21:15:37 +0700

    add modelling

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -14,54 +15,12 @@ Route::get('/about', function () {
 Route::get('/posts', function () {
     return view('posts', [
         'title' => 'Blog',
-        'posts' => [
-            [
-                'id' => 1,
-                'slug' => 'functionality-of-vuejs',
-                'title' => 'Functionality of VueJS',
-                'author' => 'Hary Capri',
-                'content' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero iure recusandae exercitationem voluptatibus
-            quia veritatis quod reiciendis facilis eaque mollitia. Facere ea nobis earum. Officiis, asperiores.
-            Distinctio placeat quam natus?Corrupti amet deleniti iste? Veniam dolores, distinctio consectetur impedit
-            officiis sint recusandae. Magni distinctio, repellat nihil, dolor reiciendis maxime explicabo inventore
-            ipsam libero illo molestiae optio deserunt aperiam tenetur. Magnam?',
-            ],
-            [
-                'id' => 2,
-                'slug' => 'what-is-react-js',
-                'title' => 'What is React JS ?',
-                'author' => 'Hary Capri',
-                'content' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam est repudiandae
-            praesentium similique et quia dolores voluptas blanditiis nihil eligendi modi corrupti aperiam voluptatibus
-            consequatur, explicabo assumenda magnam ex neque!',
-            ],
-        ],
+        'posts' =>  Post::findAll()
     ]);
 });
 
 Route::get('/post/{slug}', function ($slug) {
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'functionality-of-vuejs',
-            'title' => 'Functionality of VueJS',
-            'author' => 'Hary Capri',
-            'content' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero iure recusandae exercitationem voluptatibus
-            quia veritatis quod reiciendis facilis eaque mollitia. Facere ea nobis earum. Officiis, asperiores.
-            Distinctio placeat quam natus?Corrupti amet deleniti iste? Veniam dolores, distinctio consectetur impedit
-            officiis sint recusandae. Magni distinctio, repellat nihil, dolor reiciendis maxime explicabo inventore
-            ipsam libero illo molestiae optio deserunt aperiam tenetur. Magnam?',
-        ],
-        [
-            'id' => 2,
-            'slug' => 'what-is-react-js',
-            'title' => 'What is React JS ?',
-            'author' => 'Hary Capri',
-            'content' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam est repudiandae
-            praesentium similique et quia dolores voluptas blanditiis nihil eligendi modi corrupti aperiam voluptatibus
-            consequatur, explicabo assumenda magnam ex neque!',
-        ],
-    ];
+    $posts = Post::findAll();
 
     $post = Arr::first($posts, function ($post) use ($slug) {
         return $post['slug'] == $slug;
