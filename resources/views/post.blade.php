29baff4acef70ef87ef80076dae657c9a5f82bef
Hary Capri <76942747+harycp@users.noreply.github.com>
2024-08-23 21:52:13 +0700

    adding load eager loading

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,7 @@
     <article class="py-6 max-w-screen-md border-b-2 border-gray-400">
         <h2 class="mb-1 text-2xl font-bold tracking-tight text-gray-900">{{ $post['title'] }}</h2>
         <div class="text-gray-500 text-base mb-4">
-            <a class="hover:underline" href="/authors/{{ $post->author->id }}">{{ $post->author->name }}</a> |
+            <a class="hover:underline" href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> |
             {{ $post['created_at']->diffForHumans() }} |
             <a href="/categories/{{ $post->category->slug }}"
                 class="hover:underline text-gray-500 text-base">{{ $post->category->name }}</a>
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/about', function () {
 
 Route::get('/posts', function () {
 
-    $post = Post::with('author', 'category')->latest()->get();
+    $post = Post::with(['author', 'category'])->latest()->get();
     return view('posts', [
         'title' => 'Blog',
         'posts' =>  $post
@@ -30,12 +30,15 @@ Route::get('/post/{post:slug}', function (Post $post) {
 
 Route::get('/authors/{user:username}', function (User $user) {
 
-    return view('posts', ['title' => count($user->posts) . ' Blogs by ' . $user->name, 'posts' => $user->posts]);
+    $posts = $user->posts->load(['author', 'category']);
+
+    return view('posts', ['title' => count($posts) . ' Blogs by ' . $user->name, 'posts' => $posts]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
+    $posts = $category->posts->load(['author', 'category']);
     
-    return view('posts', ['title' => $category->name, 'posts' => $category->posts]);
+    return view('posts', ['title' => $category->name, 'posts' => $posts]);
 });
 
 Route::get('/contact', function () {
