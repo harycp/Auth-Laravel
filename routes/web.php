2a1a4d08b4898e1cb213ad6b74a90af3f3d22f89
Hary Capri <76942747+harycp@users.noreply.github.com>
2024-10-20 21:29:57 +0700

    feat: add dashboard routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
@@ -34,3 +35,5 @@ Route::get("/login", [LoginController::class, 'index']);
 Route::post('/login', [LoginController::class, 'authenticate']);
 Route::get("/register", [RegisterController::class, 'index']);
 Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/dashboard', [DashboardController::class, 'index']);
