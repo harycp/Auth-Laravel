3c140689df050729aa5303c874a9ceab77cb733b
Hary Capri <76942747+harycp@users.noreply.github.com>
2024-12-03 21:45:37 +0700

    Update Category.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,7 +10,7 @@ class Category extends Model
 {
     use HasFactory;
 
-    public function posts(): HasMany{
+    public function posts():HasMany{
         return $this->hasMany(Post::class);
     }
 }
